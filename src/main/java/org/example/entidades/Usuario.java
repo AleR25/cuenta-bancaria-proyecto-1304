25c1546abd775b9bac2f651fbf6124d31af33920
Ale Ramos <cesar.rg482@gmail.com>
2026-04-15 08:31:10 -0300

    cuentas bancarias

--- src/main/java/org/example/entidades/Usuario.java
+++ src/main/java/org/example/entidades/Usuario.java
@@ -1,4 +1,15 @@
 package org.example.entidades;
 
 public class Usuario {
+    private int idUsuario;
+    private int codRol;
+    private String username;
+    private String password;
+
+    public boolean autenticar(String username, String password)
+    {
+        return
+                this.username.equalsIgnoreCase(username)
+                && this.password.equalsIgnoreCase(password);
+    }
 }
